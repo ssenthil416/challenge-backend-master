5b1cce70f26cf179620215005c061c7b64c872f7
Sundara Senthil SN <C-Sundara.Senthil1@charter.com>
2018-04-24 08:35:24 -0400

    Fixed Build

--- challenge-eng-base-master/backend-golang/main.go
+++ challenge-eng-base-master/backend-golang/main.go
@@ -3,37 +3,41 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
+        "errors"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
-   setMaxOpenConn = 10
-   setMaxIdleConn = 0
+	setMaxOpenConn = 10
+	setMaxIdleConn = 0
 )
 
-type UserStruct struct{
-  Name string `db:"name", json:"name"`
-  Password string `db:"password", json:"passowrd"`
+type UserStruct struct {
+	Name     string `db:"name", json:"name"`
+	Password string `db:"password", json:"passowrd"`
 }
 
 /*
 Note
-  ContentMetaData 
+  ContentMetaData
     text/plain = text
     image/png  = image/width/height
     video/mpeg = video/length/source
 */
 
-type MessageStruct struct{
-   Sender string `db:"sender", json:"sender"`
-   Recipient string `db:"recipient", json:"recipient"`
-   Message string `db:"message", json:"message"`
-   ContentMetaData string `db:"contentmetadata", json:"contentmetadata"` 
-   Timestamp time `db:"timestamp", json:"timestamp"`
-} 
+type MessageStruct struct {
+	Sender          string `db:"sender", json:"sender"`
+	Recipient       string `db:"recipient", json:"recipient"`
+	Message         string `db:"message", json:"message"`
+	ContentMetaData string `db:"contentmetadata", json:"contentmetadata"`
+	Timestamp       string `db:"timestamp", json:"timestamp"`
+}
 
 func main() {
 	db, err := sql.Open("mysql", "root:testpass@tcp(db:3306)/challenge")
@@ -41,9 +45,9 @@ func main() {
 		log.Fatal("unable to connect to DB", err)
 	}
 
-        if err := createNInitialiseMySQL(db); err != nil {
-                log.Panic(err)
-        }
+	if err := createNInitialiseMySQL(db); err != nil {
+		log.Panic(err)
+	}
 
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		var result string
@@ -60,172 +64,171 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/uses",func(w http.ResponseWriter, r *http.Request) {
-                 if err := userHandler(w, r, db, "POST"); err != nil {
-                     http.Error(w, err.Error(), http.StatusInternalServerError)
-                 } else {
-                   http.Error(w, "OK", http.StatusOK) 
-                 }
-        }).Method("POST") 
-     
-	http.HandleFunc("/uses",func(w http.ResponseWriter, r *http.Request) {
-                 if err := userHandler(w, r, db, "PUT"); err != nil {
-                     http.Error(w, err.Error(), http.StatusInternalServerError)
-                 } else {
-                   http.Error(w, "OK", http.StatusOK) 
-                 }
-        }).Method("PUT") 
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		   if err := userHandler(w, r, db); err != nil {
+		       http.Error(w, err.Error(), http.StatusInternalServerError)
+		   } else {
+                     fmt.Println("err =", err)
+		     http.Error(w, "OK", http.StatusOK)
+		   }
+	})
 
-	http.HandleFunc("/messages",func(w http.ResponseWriter, r *http.Request) {
-                 if err := messageHandler(w, r, db, "POST"); err != nil {
-                     http.Error(w, err.Error(), http.StatusInternalServerError)
-                 } else {
-                   http.Error(w, "OK", http.StatusOK) 
-                 }
-        }).Method("POST") 
-     
-	http.HandleFunc("/messages",func(w http.ResponseWriter, r *http.Request) {
-                 if err := messageHandler(w, r, db, "PUT"); err != nil {
-                     http.Error(w, err.Error(), http.StatusInternalServerError)
-                 } else {
-                   http.Error(w, "OK", http.StatusOK) 
-                 }
-        }).Method("PUT") 
+	http.HandleFunc("/messages", func(w http.ResponseWriter, r *http.Request) {
+		if err := messageHandler(w, r, db); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else {
+			http.Error(w, "OK", http.StatusOK)
+		}
+	})
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func createNInitialiseMySQL(db *DB)(error){
+func createNInitialiseMySQL(db *sql.DB) error {
 
-   db.SetMaxOpebConns(setMaxOpenConn)
-   db.SetMacIdleConns(setMaxIdleConn)
+	db.SetMaxOpenConns(setMaxOpenConn)
+	db.SetMaxIdleConns(setMaxIdleConn)
 
-  	if _, err = db.Exec(CREATE_USERS); err != nil {
-		 return err
-	} 
+	if _, err := db.Exec(CREATE_USERS); err != nil {
+		return err
+	}
 
-  	if _, err = db.Exec(CREATE_MSG); err != nil {
-		 return err
-	} 
+	if _, err := db.Exec(CREATE_MSG); err != nil {
+		return err
+	}
 
-   return nil
+	return nil
 }
 
+func userHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) (err error) {
+        cmd := r.Method
+        firstTime := true
+        fmt.Println("cmd =", cmd)
+	dec := json.NewDecoder(r.Body)
+	for {
+		var t UserStruct
+		if err := dec.Decode(&t); err == io.EOF {
+                        if firstTime {
+                        return errors.New("Missing Arguments") 
+                        }
+                        break
+		} else if err != nil {
+			return err
+		}
+                firstTime = false
 
-func userHandle(w http.ResponseWriter, r *http.Request, db *DB, cmd string)(error) {
-    dec := json.NewDecoder(r.Body)
-    for {
-        var t UserStruct
-        if err := dec.Decode(&t); err == io.EOF {
-            break
-        } else if err != nil {
-	     return err
-        }
+                fmt.Println("Data t =%v", t)
+		//Insert to Table
+		var stmt *sql.Stmt
+		if cmd == "POST" {
+			stmt, err = db.Prepare(INSERT_USER)
+			if err != nil {
+				return err
+			}
 
-        //Insert to Table
-        var stmt *Stmt
-        if cmd == "POST" {
-           stmt, err := db.Prepare(INSERT_USER)
-           if err != nil {
-	     return err
-           }
+			res, err := stmt.Exec(t.Name, t.Password)
+			if err != nil {
+				return err
+			}
 
-           res, err := stmt.Exec(t.Name, t.Password)
-           if err != nil {
-	     return err
-           }
+			lid, err := res.LastInsertId()
+			if err != nil {
+				return err
+			}
 
-           _, err := res.LastInsertId()
-           if err != nil {
-	     return err
-           }
-      
+			fmt.Println("lastInsert ID =", lid)
 
-        } else if cmd == "PUT" {
-           stmt, err := db.Prepare(UPDATE_USER)
-           if err != nil {
-	     return err
-           }
+		} else if cmd == "PUT" {
+			stmt, err = db.Prepare(UPDATE_USER)
+			if err != nil {
+				return err
+			}
 
-           res, err := stmt.Exec(t.Name, t.Password)
-           if err != nil {
-	     return err
-           }
+			res, err := stmt.Exec(t.Name, t.Password)
+			if err != nil {
+				return err
+			}
 
-           _, err := res.RowsAffected()
-           if err != nil {
-	     return err
-           }
-        }
-    }   
+			reff, err := res.RowsAffected()
+			if err != nil {
+				return err
+			}
 
-    return nil
+			fmt.Println("RowsAffected =", reff)
+		}
+	}
+
+	return nil
 }
-
 
 type Content struct {
-    Sender string `db:"sender", json:"sender"`
-    Recipient string `db:"recipient", json:"recipient"`
-    Message string `db:"message", json:"message"`
+	Sender    string `db:"sender", json:"sender"`
+	Recipient string `db:"recipient", json:"recipient"`
+	Message   string `db:"message", json:"message"`
 }
 
-func messageHandler(w http.ResponseWriter, r *http.Request, db *DB, cmd string)(error) {
-    var imageWidth, imageHeight, videoLen, videoSrc string
-    contentType :=  r.Header.Get("Content-Type")
+func messageHandler(w http.ResponseWriter, r *http.Request, db *sql.DB)(err  error) {
+	var imageWidth, imageHeight, videoLen, videoSrc string
+        cmd := r.Method
+	contentType := r.Header.Get("Content-Type")
 
-    sliceMessage:=[]Make(MessageStruct)
-    for {
+	sliceMessage := make([]MessageStruct, 0, 1)
+	dec := json.NewDecoder(r.Body)
+	for {
+		var t MessageStruct
+		var content Content
+		if err := dec.Decode(&content); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
 
-        var t MessageStruct      
-        var content Content
-        if err := dec.Decode(&content); err == io.EOF {
-            break
-        } else if err != nil {
-	     return err
-        }
-    
-           t.Message = content.Message
-           t.Sender = content.Sender
-           t.Recipient = content.Recipient
-           t.TimeStamp = Time.Now().ToString()
+		t.Message = content.Message
+		t.Sender = content.Sender
+		t.Recipient = content.Recipient
+		t.Timestamp = time.Now().String()
 
-      switch contentType {
-      case "text/plain":
-           t.ContentMetaData = "text"
-      case "image/png":
-           imageWidth = r.Header.Get("width")
-           imageHeight = r.Header.Get("height")
-           t.ContentMetaData = "image"+"/"+imageWidth+"/"+iageHeight 
-      case "video/mpeg":
-           videoLen = r.Header.Get("length")
-           videoSrc = r.Header.Get("source")
-           t.ContentMetaData = "video"+"/"+videoLen+"/"+videoSrc 
-      default :
-         log.Info("Need to support Content Type =", contentType)
-      }
+		switch contentType {
+		case "text/plain":
+			t.ContentMetaData = "text"
+		case "image/png":
+			imageWidth = r.Header.Get("width")
+			imageHeight = r.Header.Get("height")
+			t.ContentMetaData = "image" + "/" + imageWidth + "/" + imageHeight
+		case "video/mpeg":
+			videoLen = r.Header.Get("length")
+			videoSrc = r.Header.Get("source")
+			t.ContentMetaData = "video" + "/" + videoLen + "/" + videoSrc
+		default:
+			log.Printf("Need to support Content Type =%v", contentType)
+		}
 
-      sliceMessage = append(sliceMessage,t) 
-    }
+		sliceMessage = append(sliceMessage, t)
+	}
 
-     var stmt *Stmt
-     if cmd == "POST" {
-           stmt, err := db.Prepare(INSERT_USER)
-           if err != nil {
-	     return err
-           }
+	var stmt *sql.Stmt
+	if cmd == "POST" {
+		stmt, err = db.Prepare(INSERT_USER)
+		if err != nil {
+			return err
+		}
+		var res sql.Result
+		for _, msg := range sliceMessage {
+			res, err = stmt.Exec(msg.Sender, msg.Recipient, msg.Message, msg.ContentMetaData, msg.Timestamp)
+			if err != nil {
+				return err
+			}
+		}
 
-           for _,msg := range sliceMessage{
-              res, err := stmt.Exec(msg.Sender, msg.Recipient, msg.Message, msg.Contentmetadata, msg.Timestamp)
-              if err != nil {
-	         return err
-              }
-           }
+		lid, err := res.LastInsertId()
+		if err != nil {
+			return err
+		}
 
-           _, err := res.LastInsertId()
-           if err != nil {
-	     return err
-           }
-     }
+		fmt.Println("LastInsertID =", lid)
+	}
+
+	return nil
 }
--- challenge-eng-base-master/backend-golang/query.go
+++ challenge-eng-base-master/backend-golang/query.go
@@ -1,8 +1,7 @@
 package main
 
 const (
-
-	CREATE_USER = `
+	CREATE_USERS = `
         	CREATE TABLE IF NOT EXISTS users (
        			name varchar(100) NOT NULL,
        			password varchar(100) NOT NULL,
@@ -10,24 +9,24 @@ const (
        		);
        `
 
-       INSERT_USER = `
+	INSERT_USER = `
                INSERT INTO TABLE users VALUES(?,?);
        `
-       UPDATE_USER = `
+	UPDATE_USER = `
                UPDATE TABLE users SET password=? WHERE name=?;
        `
-       CREATE_MSG = `
+	CREATE_MSG = `
         	CREATE TABLE IF NOT EXISTS messages (
        			sender varchar(100) NOT NULL,
        			recipient varchar(100) NOT NULL,
                         message varchar(1000),
                         contentmetadata varchar(50) NOT NULL,
                         timestamp timestamp NOT NULL,
-       			PRIMARY KEY (sender,receipient)
+       			PRIMARY KEY (sender,recipient)
        		);
        `
 
-       INSERT_MSG = `
+	INSERT_MSG = `
                INSERT INTO TABLE messages VALUES;
        `
 )
